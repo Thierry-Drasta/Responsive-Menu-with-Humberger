647eece49d3edd2e8b0b1086347c7a74d4645c3e
Thierry-Drasta <tuyishimethierry127@gmail.com>
2023-01-06 01:54:39 +0200

    adding files

---
+++ JS/app.js
@@ -0,0 +1,13 @@
+const humberger = document.querySelector(".humberger");
+const navMenu = document.querySelector(".nav-menu");
+
+humberger.addEventListener("click", () => {
+    humberger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.
+    addEventListener("click", () => {
+        humberger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }));
